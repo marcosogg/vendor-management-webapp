1c8dcafb77fea00b7b93f99a5ccc9d1903b5f356
Marcos Gomes <marcosogomes@gmail.com>
2024-07-27 14:21:45 +0100

    Your commit message explaining the changes

--- data_import/forms.py
+++ data_import/forms.py
@@ -1,11 +1,12 @@
 from django import forms
 
+
 class FileUploadForm(forms.Form):
-    file = forms.FileField(label='Select a CSV or Excel file')
-    
+    file = forms.FileField(label="Select a CSV or Excel file")
+
     def clean_file(self):
-        file = self.cleaned_data['file']
-        ext = file.name.split('.')[-1].lower()
-        if ext not in ['csv', 'xlsx', 'xls']:
-            raise forms.ValidationError('Only CSV and Excel files are allowed.')
+        file = self.cleaned_data["file"]
+        ext = file.name.split(".")[-1].lower()
+        if ext not in ["csv", "xlsx", "xls"]:
+            raise forms.ValidationError("Only CSV and Excel files are allowed.")
         return file
