5326fd1d76a7eb19ada1e909220655dbaa69ecb7
Marcos Gomes <marcosogomes@gmail.com>
2024-07-29 22:16:43 +0100

    adjustments

--- vendor_management/urls.py
+++ vendor_management/urls.py
@@ -20,11 +20,20 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('core.urls')),
-    path('import/', include('data_import.urls')),
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path("admin/", admin.site.urls),
+    path("", include("core.urls")),
+    path("import/", include("data_import.urls")),
+    path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
+    path(
+        "accounts/logout/",
+        auth_views.LogoutView.as_view(next_page="login"),
+        name="logout",
+    ),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
